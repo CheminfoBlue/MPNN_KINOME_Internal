9e81bca81e9160b03b21c7e47097ec2e3edeed77
jvenegas-blueprint <137185315+jvenegas-blueprint@users.noreply.github.com>
2024-01-16 13:53:14 -0500

    Update env name in kinomepred_parallel.sh

--- kinomepred_parallel.sh
+++ kinomepred_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source activate dilipred
+source activate KinomePred
 python ./code/pre_process_kinomescan.py ./data/kinome_scan-2023_12_15.csv
 python ./code/KinomePred_MultiProcess_batched.py > KinomePred_out.log
 python ./code/compile_results.py ./results/LGB/ --save_all_preds
