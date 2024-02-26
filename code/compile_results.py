6b2b4a42ece617d3181fadc04d3c66b3b8c50c84
Julian Venegas <jvenegas@ip-172-17-5-185.ec2.internal>
2024-02-26 13:26:55 -0500

    add static file. Modify results compile.

--- code/compile_results.py
+++ code/compile_results.py
@@ -4,9 +4,8 @@ import re
 import pandas as pd
 import numpy as np
 import argparse
-import joblib
 from argparse import *
-from learner import MultiOutputClassifier
+from learner import score_regression, score_multiclass
 
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -17,7 +16,8 @@ parser.add_argument("--save_all_preds", action='store_true', help="set flag to s
 parser.add_argument("--append_results", action='store_true', help="set flag to append results to respective current files")
 
 args=parser.parse_args()
-# args=parser.parse_args(["./results/LGB"])
+print(args)
+# args=parser.parse_args(["../results/MPNN"])
 
 '''
         (without sscore_target_map)  python compile_results.py ./results/LGB/
@@ -458,137 +458,105 @@ else:
 def is_sscore_target(x):
     return ('S(10)' in x)|('S(35)' in x)
 
-def is_res_path(p):
-    return ('_scores.csv' in p)|('_preds.csv' in p)|('_model.rds' in p)
-
 def save_results(res, res_pth, update_append=False):
     current_res = None 
     if update_append:
         try:
             current_res = pd.read_csv(res_pth)
         except:
-            print('No current/existing results file to append to \n %s DNE!'%res_pth)
+            print('No current/existing results file to append to \n %s DNE!'%p)
     current_res = pd.concat([current_res, res], axis=0)
     current_res.to_csv(res_pth, index=False)
         
-        
-kinase_info = pd.read_csv('./data/kinases-on-panel-468.csv')
-kinase_ls_master = kinase_info.Full.tolist()
-target_ls_master = kinase_ls_master+['S(10)', 'S(35)']
+def clean_mpnn_structures(smiles_ls):
+    smiles_ls_clean = [smiles[2:-2] for smiles in smiles_ls]
+    smiles_ls_clean = [smiles.replace('\\\\', '\\') for smiles in smiles_ls_clean] 
+    return smiles_ls_clean
+
+
+def str_ls_to_float_ls(x):
+    if ', ' in x:
+      delim = ', ' 
+    else:
+        delim = ' '
+        x = '['+x.strip('[]').strip()+']'
+        x = ' '.join(x.split())
+    # print('delim: ', delim)
+    return [float(val) for val in x.strip('[]').split(delim)]
+
 # results_path = './results/LGB'
 results_path = args.results_path
 
 
-# res_path_ls = [os.path.join(results_path, p) for p in os.listdir(results_path) if ('model.rds' not in p)&('_all.csv' not in p)]
-res_path_ls = [os.path.join(results_path, p) for p in os.listdir(results_path) if is_res_path(p)]
-# preds_path_ls = [p for p in res_path_ls if ('_preds.csv' in p)]
-preds_path_ls = [os.path.join(results_path, t+'_test_preds.csv') for t in target_ls_master]
-# scores_path_ls = [p for p in res_path_ls if ('_scores.csv' in p)]
-scores_path_ls = [os.path.join(results_path, t+'_test_scores.csv') for t in target_ls_master]
-# target_ls = [os.path.split(p)[-1].split('_test_preds.csv')[0] for p in preds_path_ls]
-models_path_ls = [os.path.join(results_path, t+'_model.rds') for t in target_ls_master]
-target_ls = target_ls_master
-sscore_ls = [t for t in target_ls if is_sscore_target(t)]
-kinome_ls = [t for t in target_ls if (t not in sscore_ls)]
+kinome_exp = pd.read_csv('./data/kinome_data_multiclass_current.csv')
+kinome_exp_test = kinome_exp[kinome_exp.Split=='test'].copy().reset_index(drop=True)
+del kinome_exp
+kinase_cols = kinome_exp_test.columns[~kinome_exp_test.columns.isin(['Compound Name', 'Split', 'Structure', 'S(10)', 'S(35)'])].tolist()
 
-preds_ls  = [pd.read_csv(p) for p in preds_path_ls if not is_sscore_target(p)]
-scores_ls  = [pd.read_csv(p) for p in scores_path_ls if not is_sscore_target(p)]
-kinase_pred_models = [joblib.load(p) for p in models_path_ls if not is_sscore_target(p)]
+kinome_test_preds = pd.read_csv(os.path.join(results_path, 'kinome_test_preds.csv'))
+sscore_test_preds = pd.read_csv(os.path.join(results_path, 'SScore_test_preds.csv'))
 
-sscore_preds_ls  = [pd.read_csv(p) for p in preds_path_ls if is_sscore_target(p)]
-sscore_scores_ls  = [pd.read_csv(p) for p in scores_path_ls if is_sscore_target(p)]
+kinome_test_preds.rename({'smiles': 'Structure'}, axis=1, inplace=True)
+sscore_test_preds.rename({'smiles': 'Structure'}, axis=1, inplace=True)
 
-id_cols = preds_ls[0].columns[~preds_ls[0].columns.str.contains('pred')].tolist()
-id_cols_select = id_cols.copy()
-try:
-    id_cols_select.remove('Split')
-except:
-    print('No split column present')
+kinome_test_preds.Structure = clean_mpnn_structures(kinome_test_preds.Structure.tolist())
+sscore_test_preds.Structure = clean_mpnn_structures(sscore_test_preds.Structure.tolist())
 
-try:
-    id_cols_select.remove('MolWeight')
-except:
-    print('No MolWeight column present')
+kinome_test_preds[kinase_cols]=kinome_test_preds[kinase_cols].applymap(lambda x: str_ls_to_float_ls(x))
 
-pred_label_ls = [pd.concat([p.loc[:, p.columns.isin(id_cols)], 
-                            pd.DataFrame({t: p.loc[:, ~p.columns.isin(id_cols)].idxmax(axis=1, numeric_only=True).str.split('pred_').str[-1].astype(np.int32)})], axis=1) 
-                 for t, p in zip(kinome_ls, preds_ls)]
-scores_df = pd.concat(scores_ls, axis=0).reset_index(drop=True)
-scores_df.insert(0, 'Target', kinome_ls)
-save_results(scores_df, res_pth=os.path.join(results_path, 'test_scores_all.csv'), update_append=False) 
-# scores_df.to_csv(os.path.join(results_path, 'test_scores_all.csv'), index=False)
-# print('KinomePred model scores - median over targets: ', scores_df.median(axis=0, numeric_only=True))
+#Add compound name and run date
+if all(kinome_test_preds.Structure == kinome_exp_test.Structure)==all(sscore_test_preds.Structure == kinome_exp_test.Structure):
+    print('Smiles match original test set.')
+    kinome_test_preds = pd.concat([kinome_exp_test[['Compound Name']], kinome_test_preds], axis=1)
+    kinome_test_preds.insert(0, 'run_date', current_date)
+    sscore_test_preds = pd.concat([kinome_exp_test[['Compound Name']], sscore_test_preds], axis=1)   
+    sscore_test_preds.insert(0, 'run_date', current_date)                          
+else:
+    print('Smiles do not match original test set, cannot proceed.')
 
-sscores_scores_df = pd.concat(sscore_scores_ls, axis=0).reset_index(drop=True)
-sscores_scores_df.insert(0, 'Target', sscore_ls) 
-# sscores_scores_df.to_csv(os.path.join(results_path, 'S-score_scores_all.csv'), index=False)
-save_results(sscores_scores_df, res_pth=os.path.join(results_path, 'S-score_scores_all.csv'), update_append=False) 
+
 
 if args.save_all_preds:
-    print('saving all pred probs')
-    test_preds_all = preds_ls.copy()
-    test_preds_all = [pd.concat([p.loc[:,id_cols_select], pd.DataFrame({t: p.loc[:,~p.columns.isin(id_cols)].values.tolist()})], axis=1) for t, p in zip(target_ls, preds_ls)]
-    test_preds_all = pd.concat(test_preds_all, axis=1)
+    test_preds_all = pd.concat([kinome_test_preds, sscore_test_preds], axis=1)
     test_preds_all = test_preds_all.loc[:,~test_preds_all.columns.duplicated()]
-    test_preds_all.insert(0, 'run_date', current_date)
-    # test_preds_all.to_csv(os.path.join(results_path, 'test_pred_probs_all.csv'), index=False)
     save_results(test_preds_all, res_pth=os.path.join(results_path, 'test_pred_probs_all.csv'), update_append=args.append_results)
 
-#get structure and predicted probs for class 2 (PoC<=10) only
-preds_ls = [preds.loc[:,id_cols_select+[preds.columns[-1]]] for preds in preds_ls]
-#concatenate all the kinase class 2 probabilities
-preds_df = pd.concat(preds_ls, axis=1)
-preds_df = preds_df.loc[:,~preds_df.columns.duplicated()]
-preds_df.insert(0, 'run_date', current_date)
-# preds_df.to_csv(os.path.join(results_path, 'test_pred_probs_class2_all.csv'), index=False)
-save_results(preds_df, res_pth=os.path.join(results_path, 'test_pred_probs_class2_all.csv'), update_append=args.append_results)
 
-#compile predicted kinome profile
-sscore_pred_df = pd.concat(sscore_preds_ls, axis=1)
-sscore_pred_df = sscore_pred_df.loc[:,~sscore_pred_df.columns.duplicated()]
+test_preds_class2 = kinome_test_preds.copy()
+test_preds_class2[kinase_cols] = test_preds_class2[kinase_cols].applymap(lambda x: x[2])
+save_results(test_preds_class2, res_pth=os.path.join(results_path, 'test_pred_probs_class2_all.csv'), update_append=args.append_results)
 
-kinome_pred_df = pd.concat(pred_label_ls, axis=1)
-kinome_pred_df = kinome_pred_df.loc[:,~kinome_pred_df.columns.duplicated()]
-#get selected target cols for sscore calculation
-select_targets = kinome_ls #list(sscore_target_group_dict.keys())
-sscore_df = kinome_pred_df.copy()[select_targets]
+
+
+
+kinome_pred_scores_ls =  []
+for kinase in kinase_cols:
+    score = score_multiclass(np.vstack(kinome_test_preds[kinase].values), kinome_exp_test[kinase].values, classes=np.array([0,1,2]))
+    kinome_pred_scores_ls += [score]
+kinome_pred_scores = pd.concat(kinome_pred_scores_ls, axis=0).reset_index(drop=True)
+kinome_pred_scores.insert(0, 'Target', kinase_cols)
+save_results(kinome_pred_scores, res_pth=os.path.join(results_path, 'test_scores_all.csv'), update_append=False) 
+
+
+sscore_pred_scores_ls =  []
+sscore_pred_scores_ls += [score_regression(sscore_test_preds['S(10)'].values, kinome_exp_test['S(10)'].values)]
+sscore_pred_scores_ls += [score_regression(sscore_test_preds['S(35)'].values, kinome_exp_test['S(35)'].values)]
+sscore_pred_scores = pd.concat(sscore_pred_scores_ls, axis=0).reset_index(drop=True)
+sscore_pred_scores.insert(0, 'Target', ['S(10)', 'S(35)'])
+save_results(sscore_pred_scores, res_pth=os.path.join(results_path, 'S-score_scores_all.csv'), update_append=False) 
+
+
+kinome_sscore_test_preds = kinome_test_preds.copy()
+kinome_sscore_test_preds[kinase_cols] = kinome_sscore_test_preds[kinase_cols].applymap(lambda x: np.array(x).argmax())
+
+sscore_df = kinome_sscore_test_preds.copy()[kinase_cols]
 # kinome_df = kinome_df.loc[:, ~kinome_df.columns.isin(list(set(target_ls)-set(select_targets)))]
 #group according to the kinase-eurofin map data specifications (most kinase make up their own group)
 #get within-group maximums -- applied to yield a hit in sscore calculation if there's a hit for at least one group member
 sscore_df = sscore_df.groupby(sscore_target_group_dict, axis=1).max()
 #get SScore S(k), where k is your chosen PoC cutoff (right endpoint - inclusive)
-kinome_pred_df['S(10)'] = sscore(sscore_df, k=10)
-kinome_pred_df['S(35)'] = sscore(sscore_df, k=35)
-kinome_pred_df['S(10)_direct'] = sscore_pred_df['S(10)_pred'].values
-kinome_pred_df['S(35)_direct'] = sscore_pred_df['S(35)_pred'].values
-try:
-    kinome_pred_df.drop(columns=['Split'], inplace=True)
-except:
-    print('No split column present')
-try:
-    kinome_pred_df.drop(columns=['MolWeight'], inplace=True)
-except:
-    print('No split column present')
-kinome_pred_df.insert(0, 'run_date', current_date)
-# kinome_pred_df.to_csv(os.path.join(results_path, 'kinome_pred_all.csv'), index=False)
-save_results(kinome_pred_df, res_pth=os.path.join(results_path, 'kinome_pred_all.csv'), update_append=args.append_results)
-
-
-#save combined model -- all 468 kinase pred models as a single multi-output model
-multiout_model = MultiOutputClassifier(n_outputs=len(kinase_pred_models))
-multiout_model.classifiers = kinase_pred_models
-joblib.dump(multiout_model, os.path.join(results_path, 'model_combined.rds'))
-
-for p in models_path_ls:
-    if not is_sscore_target(p):
-        os.remove(p)
-
-
-for p in preds_path_ls:
-    if not is_sscore_target(p):
-        os.remove(p)
-
-
-for p in scores_path_ls:
-    if not is_sscore_target(p):
-        os.remove(p)
+kinome_sscore_test_preds['S(10)'] = sscore(sscore_df, k=10)
+kinome_sscore_test_preds['S(35)'] = sscore(sscore_df, k=35)
+kinome_sscore_test_preds['S(10)_direct'] = sscore_test_preds['S(10)'].values
+kinome_sscore_test_preds['S(35)_direct'] = sscore_test_preds['S(35)'].values
+save_results(kinome_sscore_test_preds, res_pth=os.path.join(results_path, 'kinome_pred_all.csv'), update_append=args.append_results)
