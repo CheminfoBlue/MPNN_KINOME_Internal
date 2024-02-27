b658d5775881db56259ab83c9a390e3ab5780280
Julian Venegas <jvenegas@ip-172-17-5-185.ec2.internal>
2024-02-27 14:11:06 -0500

    created adhoc model build scripts

---
+++ slurm_job_scripts/MPNN_PoC_SScore_MultiTask_adhoc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#SBATCH --job-name=MPNN_PoC_SScore_MultiTask_adhoc
+
+#SBATCH --partition=gpu-t4-4x
+
+#SBATCH --output=MPNN_PoC_SScore_MultiTask_adhoc_out.log
+
+#SBATCH --error=MPNN_PoC_SScore_MultiTask_adhoc_error.log
+
+#SBATCH -N 1
+
+source activate chemprop-BP
+chemprop_train --data_path ./data/kinome_data_multiclass_current.csv --split_sizes 0.9 0.1 0.0 --smiles_columns Structure --target_columns 'S(10)' 'S(35)' --dataset_type regression --save_dir ./results/MPNN/SScorePred
---
+++ slurm_job_scripts/MPNN_weightedCE_Kinase_PoC_MultiTask_adhoc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+#SBATCH --job-name=MPNN_weightedCE_Kinase_PoC_MultiTask_adhoc
+
+#SBATCH --partition=gpu-t4-4x
+
+#SBATCH --output=MPNN_weightedCE_Kinase_PoC_MultiTask_adhoc_out.log
+
+#SBATCH --error=MPNN_weightedCE_Kinase_PoC_MultiTask_adhoc_error.log
+
+#SBATCH -N 1
+
+source activate chemprop-BP
+chemprop_train --data_path ./data/kinome_data_multiclass_current.csv --split_sizes 0.9 0.1 0.0 --smiles_columns Structure --ignore_columns 'Compound Name' 'Split' 'S(10)' 'S(35)' --dataset_type multiclass --multiclass_num_classes 3 --class_weighted_loss --save_dir ./results/MPNN/KinomePred
